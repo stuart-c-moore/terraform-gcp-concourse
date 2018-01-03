bf7e5aa0aadd85c00643b7d0b604a3a21ad248d2
Stuart Moore <stuart.moore@gmail.com>
2018-01-03 13:41:18 +0000

    Fix variables to be passed through to other modules

--- terraform-bosh-infrastructure.tf
+++ terraform-bosh-infrastructure.tf
@@ -2,9 +2,9 @@ module "terraform-gcp-bosh" {
   source = "github.com/migs/terraform-gcp-bosh"
   project = "${var.project}"
   region = "${var.region}"
-  prefix = "${terraform.workspace}"
-  zones = "1"
-  db-ha = false
-  service_account_name = "automated"
-  service_account_role = "roles/owner"
+  prefix = "${var.prefix}"
+  zones = "${var.zones}"
+  db-ha = "${var.db-ha}
+  service_account_name = "${var.service_account_name}"
+  service_account_role = "${var.service_account_role}"
 }
--- terraform-variables.tf
+++ terraform-variables.tf
@@ -1,3 +1,7 @@
 variable "project" { }
 variable "region" { }
 variable "prefix" { default = "default" }
+variable "zones" { default = "1" }
+variable "db-ha" { default =  false }
+variable "service_account_name" { default = "automated" }
+variable "service_account_role" { default = "roles/owner" }
