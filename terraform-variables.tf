0290aaa0a780480145fd7461b8ce67fb2bd13e7f
Stuart Moore <stuart.moore@gmail.com>
2018-01-08 10:32:55 +0000

    Add concourse DB config

--- terraform-variables.tf
+++ terraform-variables.tf
@@ -5,3 +5,30 @@ variable "zones" { default = "1" }
 variable "db-ha" { default =  false }
 variable "service_account_name" { default = "automated" }
 variable "service_account_role" { default = "roles/owner" }
+variable "db-version" { default = "MYSQL_5_7" }
+variable "database_params" {
+  type = "map"
+  default {
+    type {
+      MYSQL_5_7 = "mysql"
+      POSTGRES_9_6 = "postgres"
+    }
+    bosh-adapter {
+      MYSQL_5_7 = "mysql2"
+      POSTGRES_9_6 = "postgres"
+    }
+    port {
+      MYSQL_5_7 = "3306"
+      POSTGRES_9_6 = "5432"
+    }
+    charset {
+      MYSQL_5_7 = "utf8"
+      POSTGRES_9_6 = "UTF8"
+    }
+    collation {
+      MYSQL_5_7 = "utf8_general_ci"
+      POSTGRES_9_6 = "en_US.UTF8"
+    }
+  }
+}
+
