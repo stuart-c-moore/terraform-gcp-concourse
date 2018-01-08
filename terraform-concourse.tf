5a7b68b0cf853c32300f713e454175548bd55f77
Stuart Moore <stuart.moore@gmail.com>
2018-01-08 10:58:21 +0000

    Turn CIDR into a variable

--- terraform-concourse.tf
+++ terraform-concourse.tf
@@ -1,5 +1,5 @@
 resource "google_compute_subnetwork" "concourse" {
   name = "${var.prefix}-concourse"
-  ip_cidr_range = "10.244.15.0/30"
+  ip_cidr_range = "${var.concourse-cidr}"
   network = "${module.terraform-gcp-bosh.bosh-network-link}"
 }
