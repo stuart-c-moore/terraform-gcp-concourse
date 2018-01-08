8253c1b068e1a32870f14321178408f2d59ea629
Stuart Moore <stuart.moore@gmail.com>
2018-01-08 16:09:40 +0000

    Add concourse.properties file

--- terraform-concourse-properties.tf
+++ terraform-concourse-properties.tf
@@ -6,6 +6,6 @@ export BOSH_DB_CONCOURSE_PORT=$${concourse-password}
 EOF
   vars {
     concourse-user = "${google_sql_user.concourse.name}"
-    concourse-password = "${random_string.concourse-password.result
+    concourse-password = "${random_string.concourse-password.result}"
   }
 }
