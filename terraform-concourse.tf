e73c6f03216b4a2c9a8d15b8ccf3d68e268a16e4
Stuart Moore <stuart.moore@gmail.com>
2018-01-08 11:00:56 +0000

    Add region name to concourse subnet

--- terraform-concourse.tf
+++ terraform-concourse.tf
@@ -1,5 +1,5 @@
 resource "google_compute_subnetwork" "concourse" {
-  name = "${var.prefix}-concourse"
+  name = "${var.prefix}-concourse-${var.region}"
   ip_cidr_range = "${var.concourse-cidr}"
   network = "${module.terraform-gcp-bosh.bosh-network-link}"
 }
