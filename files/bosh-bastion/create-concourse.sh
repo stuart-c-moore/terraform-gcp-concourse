20f82e015cdd796ce84f93f47b27d9b5fee4c5f0
Stuart Moore <stuart.moore@gmail.com>
2018-01-08 11:28:28 +0000

    First draft of concourse deployment scripts

---
+++ files/bosh-bastion/create-concourse.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+bosh alias-env ${project_id} -e 10.0.0.6 --ca-cert <(bosh int director-creds.yml --path /director_ssl/ca)
+eval $(./login.sh)
+
+if [[ ! -d concourse-deployment ]]; then
+    git clone https://github.com/concourse/concourse-deployment.git
+else
+    cd concourse-deployment
+    git pull
+    cd ..
+fi
--- terraform-concourse.tf
+++ terraform-concourse.tf
@@ -3,3 +3,14 @@ resource "google_compute_subnetwork" "concourse" {
   ip_cidr_range = "${var.concourse-cidr}"
   network = "${module.terraform-gcp-bosh.bosh-network-link}"
 }
+
+resourse "null_resource" "bosh-bastion" {
+  provisioner "file" {
+    source = "${path.module}/files/bosh-bastion/"
+    destination = "${var.home}/"
+    connection {
+      user = "vagrant"
+      private_key = "${var.ssh-privatekey == "" ? file("${var.home}/.ssh/google_compute_engine") : var.ssh-privatekey}"
+    }
+  }
+}
