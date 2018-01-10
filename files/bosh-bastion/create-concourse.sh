a750422f0c88bdda68c09edaa2f54b8274ae8551
Stuart Moore <stuart.moore@gmail.com>
2018-01-10 09:36:50 +0000

    Enhanced create-concourse.sh

--- files/bosh-bastion/create-concourse.sh
+++ files/bosh-bastion/create-concourse.sh
@@ -12,3 +12,19 @@ else
 fi
 
 cd concourse-deployment/cluster
+
+bosh deploy -d concourse concourse.yml \
+  -l ../versions.yml \
+  --vars-store ../../concourse-creds.yml \
+  -o operations/no-auth.yml \
+  --var external_url=http://concourse.migs.wtf:8080 \
+  --var network_name=default \
+  --var web_vm_type=default \
+  --var db_vm_type=default \
+  --var worker_vm_type=default \
+  --var db_persistent_disk_type=10GB \
+  --var deployment_name=concourse
+
+cd ../..
+
+fly -t ci login -c http://10.10.0.2:8080
