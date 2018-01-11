5c8860b89aa64b837b663968edfc4de5c716fb63
Stuart Moore <stuart.moore@gmail.com>
2018-01-11 09:48:55 +0000

    More fiddling

--- terraform-concourse.tf
+++ terraform-concourse.tf
@@ -5,7 +5,6 @@ resource "google_compute_subnetwork" "concourse" {
 }
 
 resource "null_resource" "bosh-bastion" {
-  depends_on = ["${module.terraform-gcp-bosh.bosh-bastion-private-ip}"]
   provisioner "file" {
     source = "${path.module}/files/bosh-bastion/"
     destination = "${var.home}/"
