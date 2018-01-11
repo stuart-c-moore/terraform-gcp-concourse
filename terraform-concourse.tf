e5c208f2fc2597e6a9d208f12bbd4fa9b68898f2
Stuart Moore <stuart.moore@gmail.com>
2018-01-11 10:08:55 +0000

    Removed directory file transfer

--- terraform-concourse.tf
+++ terraform-concourse.tf
@@ -6,26 +6,6 @@ resource "google_compute_subnetwork" "concourse" {
 
 resource "null_resource" "bosh-bastion" {
   provisioner "file" {
-    source = "${path.module}/files/bosh-bastion/"
-    destination = "${var.home}/"
-    connection {
-      user = "vagrant"
-      host = "${module.terraform-gcp-bosh.bosh-bastion-public-ip}"
-      private_key = "${var.ssh-privatekey == "" ? file("${var.home}/.ssh/google_compute_engine") : var.ssh-privatekey}"
-    }
-  }
-/*
-  provisioner "file" {
-    content = "${data.template_file.concourse-properties.rendered}"
-    destination = "${var.home}/concourse.properties"
-    connection {
-      user = "vagrant"
-      host = "${module.terraform-gcp-bosh.bosh-bastion-public-ip}"
-      private_key = "${var.ssh-privatekey == "" ? file("${var.home}/.ssh/google_compute_engine") : var.ssh-privatekey}"
-    }
-  }
-*/
-  provisioner "file" {
     content = "${data.template_file.concourse-create.rendered}"
     destination = "${var.home}/create-concourse.sh"
     connection {
